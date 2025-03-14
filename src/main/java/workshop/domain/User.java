e2453bf49fe29c58f39af2ea47fcf7a47a4da67d
ErosAlmeida <erosvilela.empresa@gmail.com>
2025-03-14 08:33:51 -0400

    Teste

--- src/main/java/workshop/domain/User.java
+++ src/main/java/workshop/domain/User.java
@@ -14,6 +14,7 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	private String teste;
 	
 	public String getId() {
         return id;

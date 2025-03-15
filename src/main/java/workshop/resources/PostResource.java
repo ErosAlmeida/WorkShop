a1128ea0e460c7dbfeba08c405afa413a564a2a5
ErosAlmeida <eros.almeida.vilela@gmail.com>
2025-03-15 11:29:21 -0400

    Update

--- src/main/java/workshop/resources/PostResource.java
+++ src/main/java/workshop/resources/PostResource.java
@@ -1,5 +1,6 @@
 package workshop.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import workshop.domain.Post;
 import workshop.resourceUtil.URL;
 import workshop.services.PostService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -35,5 +33,17 @@ public class PostResource {
        List<Post> list = service.findByTitle(text);
         return ResponseEntity.ok().body((list)); 
     }
+    
+    @RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> fullSearch(
+ 			@RequestParam(value="text", defaultValue="") String text,
+ 			@RequestParam(value="minDate", defaultValue="") String minDate,
+ 			@RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 }
 
